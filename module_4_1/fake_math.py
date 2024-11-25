bb5b55ac583b49424bde26279c414faa80f51ecc
lonenomad66 <op1966@gmail.com>
2024-11-25 18:43:32 +0300

    Add files via upload

---
+++ module_4_1/fake_math.py
@@ -0,0 +1,12 @@
+#  Copyright (c) 24.11.2024, 14:43
+#  Поликарпов О.Е.
+#  learn
+#  Практическое задание по курсу "Разработчик Python"
+#
+#
+
+def fake_divide(first, second):
+    if second == 0:
+        return 'Ошибка деления на ноль'
+    else:
+        return first/second
---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 24.11.2024, 14:43
+#  Поликарпов О.Е.
+#  learn
+#  Практическое задание по курсу "Разработчик Python"
+#
+#
+from fake_math import *
+from true_math import *
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
