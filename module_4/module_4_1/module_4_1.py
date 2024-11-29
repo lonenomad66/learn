52df9c0903a1dad23eca2e886a673590702f432a
lonenomad66 <op1966@gmail.com>
2024-11-29 22:55:59 +0300

    Add files via upload

---
+++ module_4/module_4_1/module_4_1.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 24.11.2024, 14:43
+#  Поликарпов О.Е.
+#  learn
+#  Практическое задание по курсу "Разработчик Python"
+#
+#
+import fake_math as scool
+import true_math as univer
+
+result1 = scool.divide(69, 3)
+result2 = scool.divide(3, 0)
+result3 = univer.divide(49, 7)
+result4 = univer.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ module_4/module_4_1/true_math.py
@@ -0,0 +1,13 @@
+#  Copyright (c) 24.11.2024, 14:43
+#  Поликарпов О.Е.
+#  learn
+#  Практическое задание по курсу "Разработчик Python"
+#
+#
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first/second
